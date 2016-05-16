375cd5dc8391e41a63f6afe7d5f3e24bae030992
Sandy Vanderbleek <sandy@air.local>
2016-05-16 15:54:35 -0500

    It works.. *~*~magic~*~*

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,14 @@
 module Main where
 
+import qualified System.IO.Silently as Sys
+import qualified Environment as Env
+import qualified Eval.Code as Elm
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = eval "1" >>= print
+
+eval :: String -> IO String
+eval s = fst <$> Sys.capture (run s)
+
+run :: String -> IO ()
+run s = Env.run (Env.empty "elm-make" "node") (Elm.eval (Nothing, s))
