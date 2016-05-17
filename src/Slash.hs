ced2ba2dd7010c14fac9d699da62b2edcde7e250
Sandy Vanderbleek <sandy@air.local>
2016-05-17 18:47:22 -0500

    Respond with JSON or empty body

--- src/Slash.hs
+++ src/Slash.hs
@@ -5,14 +5,14 @@ module Slash (app, Command(..), Response(..), ResponseType(..)) where
 import Network.HTTP.Types (status200)
 import Network.Wai (Application, Request, responseLBS)
 import Network.Wai.Parse (Param, parseRequestBody, lbsBackEnd)
-import Data.Map (Map)
 import Data.Maybe (fromMaybe)
-import qualified Data.Map as Map
+import Data.Aeson (ToJSON(..), object, (.=), encode)
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as S
 import qualified Data.ByteString.Lazy as LBS
 import qualified Data.ByteString.Lazy.Char8 as LS
 
+type Handler = (Command -> IO Response)
 type CommandText = String
 type ResponseText = String
 
@@ -28,16 +28,27 @@ data SlackRequest
 data ResponseType 
   = InChannel 
   | Ephemeral
+  | SSLChecked
 
 data Response = 
   Response ResponseText ResponseType
 
-app :: (Command -> IO Response) -> Application
+instance ToJSON ResponseType where
+  toJSON InChannel = "in_channel"
+  toJSON Ephemeral = "ephemeral"
+
+instance ToJSON Response where
+  toJSON (Response text responseType) =
+    object 
+    [ "text" .= text
+    , "response_type" .= responseType ]
+
+app :: Handler -> Application
 app handler request respond =
   do 
     params <- parse request
-    let slackRequest = mkSlackRequest params
-    respond (responseLBS status200 [] (LS.pack (show slackRequest)))
+    response <- mkResponse handler (mkRequest params)
+    respond $ responseLBS status200 [] (toLBS response)
 
 parse :: Request -> IO [Param]
 parse request = 
@@ -53,12 +64,24 @@ sslCheck params =
     "0" -> False
     "1" -> True
 
-mkSlackRequest :: [Param] -> SlackRequest
-mkSlackRequest params =
+mkCommand :: [Param] -> Command
+mkCommand params =
+  Command $ S.unpack (param ("text","") params)
+
+mkRequest :: [Param] -> SlackRequest
+mkRequest params =
   if sslCheck params 
     then SSLCheck
     else CMD $ mkCommand params 
 
-mkCommand :: [Param] -> Command
-mkCommand params =
-  Command $ S.unpack (param ("text","") params)
+mkResponse :: Handler -> SlackRequest -> IO Response
+mkResponse handler request =
+  case request of
+    SSLCheck -> return $ Response mempty SSLChecked
+    (CMD command) -> handler command
+
+toLBS :: Response -> LS.ByteString
+toLBS response@(Response text responseType) = 
+  case responseType of 
+    SSLChecked -> LS.pack text
+    _ -> encode response
